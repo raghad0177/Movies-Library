98a2919a95182bbb19540f0a0cae59526abb6034
raghad0177 <abdullahraghad616@gmail.com>
2024-04-27 15:08:05 +0000

    final all

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 CREATE TABLE movie(
     ID SERIAL PRIMARY KEY,
-    Name varchar(50),
+    original_title varchar(255),
+    title varchar(50),
+    release_date varchar(100),
+    poster_path varchar(50),
+    overview varchar(100),
     commints varchar(250)
 );
